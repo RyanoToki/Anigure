0aff84494869b4cfadece3c487edbd2c4f5e4f52
RyanoToki <hidayatrian287@gmail.com>
2024-01-04 17:56:18 +0800

    Add more products and add ellipses on title

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 (() => {
     window.onload = () => {
         setTimeout(() => {
-            document.querySelector(".loading")
-                .classList.add("hidden");
+            document.querySelector(".loading").classList.add("hidden");
         }, 1000);
     }
     
     const dummy = document.querySelector(".product");
     const parent = dummy.parentElement;
-    const format = new Intl.NumberFormat("id-ID", {
-        minimumFractionDigits: 0
-    });
+    const format = new Intl.NumberFormat("id-ID");
     
     fetch("database.json")
-    .then(res => res.json())
-    .then(res => {
-        for (const item of res) {
-            const product = dummy.cloneNode(true);
-            
-            product.querySelector(".thumb img").src = "img/products/" + item.images[0];
-            product.querySelector(".product-title").innerHTML = item.name;
-            product.querySelector(".from").innerHTML = item.from;
-            product.querySelector(".price").innerHTML = "Rp " + format.format(item.price);
-            
-            parent.appendChild(product);
-        }
-    });
+        .then(res => res.json())
+        .then(res => {
+            for (const item of res) {
+                const product = dummy.cloneNode(true);
+                
+                product.querySelector(".thumb img").src = (item.image.includes("http"))
+                    ? item.image
+                    : "img/products/" + item.image;
+                product.querySelector(".product-title").innerHTML = item.name;
+                product.querySelector(".from").innerHTML = item.from;
+                product.querySelector(".price").innerHTML = "Rp " + format.format(item.price);
+                
+                parent.appendChild(product);
+            }
+        });
 })();
